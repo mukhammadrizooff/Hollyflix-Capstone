c54e0daea13f5d9a98fb84b6967922c979aba6fa
mukhammadrizooff <legend.it@bk.ru>
2022-02-16 22:25:11 +0500

    Test the linters

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,4 +1,5 @@
-/* eslint no-undef: "error" */
+/*global getComment, id*/
+/*eslint no-undef: "error"*/
 const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
 const appID = 'hXV7QwFcLUFDBIBIVZim';
 

aa130163bf19816558a6f93c8da3a24e89a45a70
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-02-18 19:14:18 -0200

    reservation test

---
+++ src/__tests__/reservation.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import { reservationsCounter } from '../modules/reservation.js'
+
+
+test('Test the reservations counter', () => {
+    const reservationTest = [{
+            username: 'sam',
+            date_start: '2121-02-12',
+            date_end: '2121-03-21'
+        },
+        {
+            username: 'matt',
+            date_start: '2121-01-12',
+            date_end: '2121-07-22'
+        },
+        {
+            username: 'rizo',
+            date_start: '2121-03-12',
+            date_end: '2121-05-20'
+        },
+    ];
+    const count = reservationsCounter(reservationTest);
+    expect(count).toBe(3);
+});
